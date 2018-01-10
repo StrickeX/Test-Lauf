caf0af6eb28f4ed1ec25bdbead79a79a48de469d
StrickeX <32539745+StrickeX@users.noreply.github.com>
2018-01-10 20:40:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+log = function(){
+document.getElementById("container-reg").style.display = "none";
+document.getElementById("container-log").style.display = "flex";
+}
+
+reg = function(){
+document.getElementById("container-log").style.display = "none";
+document.getElementById("container-reg").style.display = "flex";
+}
+
+back = function(){
+document.getElementById("container-reg").style.display = "none";
+document.getElementById("container-log").style.display = "flex";
+}
+$("#link").hide();
