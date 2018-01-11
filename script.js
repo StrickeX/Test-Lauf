a39c29f8ab823e5a40cfb787e7d6ae986ff97b6b
StrickeX <32539745+StrickeX@users.noreply.github.com>
2018-01-11 11:51:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-src ="https://code.jquery.com/jquery-3.2.1.min.js";
+
 
 log = function(){
 document.getElementById("container-reg").style.display = "none";
-  $('main').addClass('blur');
 document.getElementById("container-log").style.display = "flex";
 }
 
